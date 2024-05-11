35be70a8950e22f8a6419bb540bbe984af87e3ea
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-05-11 10:50:43 +0200

    feature: create a function to calculate total time based on event name

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # import datetime
-import os.path
 import os
+import os.path
+from dataclasses import dataclass
 from typing import List
 
 from dotenv import load_dotenv
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from typing import List, Dict
+from collections import defaultdict
+from datetime import datetime, timedelta
 from dataclasses import dataclass
+
 
 load_dotenv()
 
@@ -70,6 +74,26 @@ def main():
         print(f"An error occurred: {error}")
 
 
+def calculate_total_time_grouped_by_event_name(events: List[Event]) -> Dict[str, float]:
+    total_time_by_event = defaultdict(float)
+
+    for event in events:
+        start_time = datetime.fromisoformat(event.start)
+        end_time = datetime.fromisoformat(event.end)
+        duration_seconds = (end_time - start_time).total_seconds()
+        total_time_by_event[event.title] += duration_seconds / 3600
+
+    return dict(total_time_by_event)
+
+
+def calculate_total_time_grouped_by_month(events: List[Event]) -> dict:
+    pass
+
+
+def calculate_total_time_grouped_by_day(events: List[Event]) -> dict:
+    pass
+
+
 if __name__ == "__main__":
     calendar = main()
 
@@ -77,3 +101,6 @@ if __name__ == "__main__":
 
     for event in calendar.events:
         print(f"{event.title} - {event.start} - {event.end}")
+
+    total_time_by_event_name = calculate_total_time_grouped_by_event_name(calendar.events)
+    print(total_time_by_event_name)
